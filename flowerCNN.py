80ed1358c1e901577ffae7b8ab4521082aefba66
mchrisnach <11012504@stud.hochschule-heidelberg.de>
2020-07-13 13:26:48 +0200

    Updated Model, added train/test split

--- flowerCNN.py
+++ flowerCNN.py
@@ -13,28 +13,42 @@ from tensorflow_core.python.keras.layers import Dropout
 
 batch_size = 128
 #number of epochs: pass the entire dataset through the netowork
-epochs = 40
+epochs = 15
 #Preset the img height and width
 IMG_HEIGHT = 64
 IMG_WIDTH = 64
 
-train_image_generator = ImageDataGenerator(rescale=1./255)
-validation_image_generator = ImageDataGenerator(rescale=1./255)
-test_image_generator = ImageDataGenerator(rescale=1./255)
+#train_image_generator = ImageDataGenerator(rescale=1./255)
+#validation_image_generator = ImageDataGenerator(rescale=1./255)
+#test_image_generator = ImageDataGenerator(rescale=1./255)
 
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory='data/train',
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           class_mode='categorical')
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-                                                              directory='data/validation',
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                              class_mode='categorical')
-test_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
-                                                              directory='data/test',
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                              class_mode='categorical')
+#Train Test Split with the test folder (validation split)
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, vertical_flip=True, validation_split=0.2)
+
+train_generator = train_datagen.flow_from_directory(directory='data/train',
+                                                    target_size=(IMG_HEIGHT,IMG_WIDTH),
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    subset = 'training')
+validation_generator = train_datagen.flow_from_directory(directory='data/train',
+                                                    target_size=(IMG_HEIGHT,IMG_WIDTH),
+                                                    batch_size=batch_size,
+                                                    class_mode='categorical',
+                                                    subset = 'validation')
+
+# train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                            directory='data/train',
+#                                                            shuffle=True,
+#                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
+#                                                            class_mode='categorical')
+# val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                               directory='data/validation',
+#                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
+#                                                               class_mode='categorical')
+# test_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                               directory='data/test',
+#                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
+#                                                               class_mode='categorical')
 
 model = Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
@@ -43,8 +57,10 @@ model = Sequential([
     MaxPooling2D(),
     Conv2D(64, 3, padding='same', activation='relu'),
     MaxPooling2D(),
+    Conv2D(128, 3, padding='same', activation='relu'),
+    MaxPooling2D(),
     Flatten(),
-    Dense(512, activation='relu'),
+    Dense(500, activation='relu'),
     Dense(300, activation='relu'),
     Dense(128, activation='relu'),
     Dense(5, activation='softmax')
@@ -55,25 +71,18 @@ model.compile(optimizer='adam',
                metrics=['accuracy'])
 model.summary()
 history = model.fit(
-    train_data_gen,
+    train_generator,
     epochs=epochs,
-    validation_data=val_data_gen,
-
+    validation_data= validation_generator
 )
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss=history.history['loss']
-val_loss=history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
-
-test_loss, test_acc = model.evaluate(test_data_gen, verbose=2)
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+test_loss, test_acc = model.evaluate(train_generator, verbose=2)
 print(test_acc)
