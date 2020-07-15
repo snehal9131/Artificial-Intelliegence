6de447d9a956d695a8e6883531d9a0115af6a0d8
mchrisnach <11012504@stud.hochschule-heidelberg.de>
2020-07-15 23:22:57 +0200

    Added code from Guhle, added output image with prediction

--- flowerCNN.py
+++ flowerCNN.py
@@ -1,29 +1,19 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, BatchNormalization
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow_core.python.keras import Input, Model
-
-import os
-import numpy as np
 import matplotlib.pyplot as plt
 
-#number of samples propagated through the network per iteration
-from tensorflow_core.python.keras.layers import Dropout
-
-batch_size = 128
+batch_size = 256
 #number of epochs: pass the entire dataset through the netowork
-epochs = 15
+epochs = 100
 #Preset the img height and width
 IMG_HEIGHT = 64
 IMG_WIDTH = 64
 
-#train_image_generator = ImageDataGenerator(rescale=1./255)
-#validation_image_generator = ImageDataGenerator(rescale=1./255)
-#test_image_generator = ImageDataGenerator(rescale=1./255)
-
 #Train Test Split with the test folder (validation split)
-train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, vertical_flip=True, validation_split=0.2)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range= 20, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, validation_split=0.2)
 
 train_generator = train_datagen.flow_from_directory(directory='data/train',
                                                     target_size=(IMG_HEIGHT,IMG_WIDTH),
@@ -34,25 +24,12 @@ validation_generator = train_datagen.flow_from_directory(directory='data/train',
                                                     target_size=(IMG_HEIGHT,IMG_WIDTH),
                                                     batch_size=batch_size,
                                                     class_mode='categorical',
-                                                    subset = 'validation')
-
-# train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-#                                                            directory='data/train',
-#                                                            shuffle=True,
-#                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
-#                                                            class_mode='categorical')
-# val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-#                                                               directory='data/validation',
-#                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
-#                                                               class_mode='categorical')
-# test_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
-#                                                               directory='data/test',
-#                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
-#                                                               class_mode='categorical')
+                                                    subset = 'validation',
+                                                    shuffle= False)
 
 model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-    BatchNormalization(),
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+    MaxPooling2D(),
     Conv2D(32, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Conv2D(64, 3, padding='same', activation='relu'),
@@ -60,21 +37,21 @@ model = Sequential([
     Conv2D(128, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Flatten(),
-    Dense(500, activation='relu'),
-    Dense(300, activation='relu'),
     Dense(128, activation='relu'),
     Dense(5, activation='softmax')
-])
 
+])
 model.compile(optimizer='adam',
-               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-               metrics=['accuracy'])
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 model.summary()
 history = model.fit(
     train_generator,
     epochs=epochs,
-    validation_data= validation_generator
+    validation_data=validation_generator
 )
+model.save('cnn.h5')
+
 print(history.history.keys())
 # summarize history for accuracy
 plt.plot(history.history['accuracy'])
@@ -84,5 +61,25 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+
 test_loss, test_acc = model.evaluate(train_generator, verbose=2)
 print(test_acc)
+
+X = validation_generator.next()
+Xarr = np.array(X[0])
+for i in range(5):
+    X = validation_generator.next()
+    np.append(Xarr,(X[0]))
+print (Xarr)
+Yarr = np.array(validation_generator.labels)
+#for i in range(len(X)):
+print(Xarr[0].shape)
+print (Yarr)
+plt.clf()
+
+img_nr = 3
+plt.imshow(Xarr[img_nr])
+Ypred = model.predict_classes(Xarr[img_nr].reshape(1,64,64,3))
+plt.title("Prediction:" + str(Ypred) + " Actual Class: " + str(Yarr[3]))
+plt.figtext(0.2,0.02,"Daisy = 0, Dandelion = 1, Rose = 2, Sunflower = 3, Tulip = 4")
+plt.show()
